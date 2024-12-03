b2a266370d95bbcb4fc9ee0c7b9d01820c8afc74
Jenga04 <jeremyja@msmk.university>
2024-12-03 20:49:20 +0100

    Agregar archivos de proyecto.

---
+++ Cifrado.cpp
@@ -0,0 +1,19 @@
+﻿// Cifrado.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Cifrado.h"
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+ cout << "Seleccione una opción: \n";
+ cout << "1: Lista de entradas";
+ cout << "2: Añadir una entrada";
+ cout << "3: Borrar una entrada";
+ cout << "4: Borrar archivo";
+ cout << "0: Salir";
+}
+
+
