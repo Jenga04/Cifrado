b2e3f4b26b3066bfe42fe2b4563394f5fe5153d5
03Fede <federicoonnis2003@gmail.com>
2024-12-10 13:02:45 +0100

    Opción 3: borrar entrada.

--- Cifrado.cpp
+++ Cifrado.cpp
@@ -43,3 +43,42 @@ void añadirEntrada(const string& nombreArchivo) {
         cout << "No se pudo abriri el archivo." << endl;
     }
 }
+
+void borrarEntrada(const string& nombreArchivo) {
+    ifstream archivo(nombreArchivo);
+    if (!archivo.is_open()) {
+        cout << "No se pudo abrir el archivo." << endl;
+        return;
+    }
+
+    vector<string> lineas;
+    string linea;
+    while (getline(archivo, linea)) {
+        lineas.push_back(linea);
+    }
+    archivo.close();
+
+    listarEntradas(nombreArchivo);
+    cout << "Introduce el número de la línea que deseas borrar: ";
+    int numLinea;
+    cin >> numLinea;
+
+    if (numLinea < 1 || numLinea > static_cast<int>(lineas.size())) {
+        cout << "Número de línea no válido." << endl;
+        return;
+    }
+
+    ofstream archivoSalida(nombreArchivo, ios::trunc);
+    if (archivoSalida.is_open()) {
+        for (size_t i = 0; i < lineas.size(); ++i) {
+            if (i != static_cast<size_t>(numLinea - 1)) { // Omitir la línea seleccionada
+                archivoSalida << lineas[i] << endl;
+            }
+        }
+        archivoSalida.close();
+        cout << "Línea borrada correctamente." << endl;
+    }
+    else {
+        cout << "No se pudo escribir en el archivo." << endl;
+    }
+}
